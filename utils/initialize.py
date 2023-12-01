90dbe96a9ee35fb8bc19c9124ea417af6f440ca1
matt <matt@mattkingsbury.com>
2023-12-01 06:08:05 -0500

    Added functionality to make group.

--- utils/initialize.py
+++ utils/initialize.py
@@ -6,14 +6,12 @@ from configs import get_config_path, set_config_path
 from entities import Server, ServerGroup
 
 
-
 class SSHAMan:
     def __init__(self, config_path=get_config_path()):
         # If a custom config path is provided, overwrite the global variable with the custom path.
         if config_path != get_config_path():
             set_config_path(config_path)
         self.config_path = config_path
-
 
     def list_all(self):
         """List all groups"""
@@ -39,12 +37,75 @@ class SSHAMan:
                 if len(dirs) == 0 and depth != 0:
                     print('')
 
+    def make_group(self, group_name):
+        """
+        Make a group of servers.
+        :param group_name: Name of the group to make. Can be a list of servers separated by '.' notation, e.g.
+                           'group1.subgroup1.subgroup2'
 
+        """
+        groups = group_name.split('.')
+        for i, group in enumerate(groups):
+            if i == 0:
+                parent_group = ServerGroup(group_name=group, sshaman_path=self.config_path)
+            else:
+                subgroup = ServerGroup(group_name=group, sshaman_path=self.config_path, relative_path=groups[i - 1])
+                parent_group.make_child(subgroup.group_name)
+                parent_group = subgroup
+
+        return parent_group
+
+    def add_server(self, group_path, alias, host, **kwargs):
+        """
+        Add a server to a group.
+        :param alias:
+        :param host:
+        :param kwargs:
+        :return:
+        """
+        last_group = self.make_group(group_path)
+
+        # Create a server and add it to the last group
+        server_data = {'alias': alias, 'host': host, **kwargs}
+        server = Server(**server_data)
+        last_group.add_server(server)
+
+
+def dev_list_all():
+    home_dir = os.path.expanduser('~')
+    test_config_path = os.path.join(home_dir, '.config', 'test_sshaman')
+    smn = SSHAMan(config_path=test_config_path)
+    smn.list_all()
+
+
+def dev_make_group():
+    home_dir = os.path.expanduser('~')
+    test_config_path = os.path.join(home_dir, '.config', 'test_sshaman')
+    smn = SSHAMan(config_path=test_config_path)
+    # smn.make_group('group1')
+    smn.make_group(group_path='group1.sg1')
+
+
+def dev_add_server():
+    home_dir = os.path.expanduser('~')
+    test_config_path = os.path.join(home_dir, '.config', 'test_sshaman')
+    smn = SSHAMan(config_path=test_config_path)
+
+    smn.add_server(
+        group_path='group1.sg1',
+        alias='vm3',
+        host='192.168.1.100',
+        port=22,
+        user='root',
+        identity_file='~/.ssh/id_rsa',
+        password='12345',
+        forward_ports=['80:localhost:8080', '443:localhost:8443'],
+        start_commands=["echo 'hello world'", "ls -la"],
+        server_group_path='/home/matt/.config/test_sshaman/group1/sg1'
+    )
 
 
 if __name__ == '__main__':
-    home_dir = os.path.expanduser('~')
-    test_config_path = os.path.join(home_dir, '.config', 'test_sshaman')
-    print(test_config_path)
-    smn = SSHAMan(config_path=test_config_path)
-    smn.list_all()
+    # dev_list_all()
+    # dev_make_group()
+    dev_add_server()
